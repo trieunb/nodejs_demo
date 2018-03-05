4f93ed9b59e1df62d2706df6774a6265b1f08a1c
trieunb <trieunb08@gmail.com>
2018-03-05 22:49:27 +0700

    update code

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var path		=	require('path');
 var bodyParser	=	require('body-parser');
 var mysql 		= 	require('mysql');
 var io 			= 	require('socket.io');
+var session = require('express-session')
+
 
 var events = require('events');
 var eventEmitter = new events.EventEmitter();
@@ -15,6 +17,8 @@ con.connect();
 var app			=	express();
 
 app.use(bodyParser());
+app.use(session({secret: 'ssshhhhh'}));
+
 // app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'controllers')));
 app.use(express.static(path.join(__dirname, 'models')));
@@ -27,7 +31,7 @@ app.set('views', path.join(__dirname, 'views'));
 // route our app
 // var router = require('./config/routes');
 // app.use('/', router);
-
+var sess;
 app.get('/login', function(req, res) {
 	res.render('login');
 	// res.send('hello, express');
@@ -35,10 +39,13 @@ app.get('/login', function(req, res) {
 
 app.post('/login', function(req, res) {
 	var phone	=	req.body.phone;
-	con.query("SELECT * FROM tb_user WHERE user_id = '"+phone+"'", function (err, result, fields) {
+	var sql 	=	"SELECT * FROM tb_user WHERE user_id = ?";
+	con.query(sql, [phone], function (err, result, fields) {
 	    if (err) throw err;
 	    console.log(result.length)
 	    if (result.length != 0 && result[0].user_id !== '') {
+	    	sess=req.session;
+	    	sess.user 	=	result[0].user_id;
 			return res.redirect('/phones');
 		}
 		res.redirect('/login');
@@ -46,7 +53,8 @@ app.post('/login', function(req, res) {
 });
 
 app.get('/phones', function(req, res) {
-	con.query("SELECT * FROM tb_user", function (err, result, fields) {
+	var sql = "SELECT * FROM tb_user";
+	con.query(sql, function (err, result, fields) {
 	    if (err) throw err;
 		res.render('phone', {
 			phones : result
@@ -55,9 +63,14 @@ app.get('/phones', function(req, res) {
 });
 
 app.get('/message/:phoneId', function(req, res) {
-	var phone 	=	req.params.phoneId;
-	res.render('message', {
-		phone : phone
+	var phone 		=	req.params.phoneId;
+	var phone_login	=	sess.user;
+	var sql = 'SELECT * FROM tb_message WHERE (user_own = ? AND user_receive = ?) OR (user_own = ? AND user_receive = ?)';
+	con.query(sql, [phone, phone_login, phone_login, phone], function (err, result, fields) {
+	    if (err) throw err;
+		res.render('message', {
+			mess : result
+		});
 	});
 });
 
