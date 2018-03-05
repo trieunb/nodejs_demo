13b4cf72177705f3ad020eb1d91da1a0d7e82ba3
trieunb <trieunb08@gmail.com>
2018-03-05 22:50:25 +0700

    update code

--- models/nodejs.sql
+++ models/nodejs.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 04, 2018 at 12:02 PM
+-- Generation Time: Mar 05, 2018 at 04:49 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.21
 
@@ -27,10 +27,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tb_message` (
-  `message_id` varchar(20) DEFAULT NULL,
-  `user_id` varchar(15) DEFAULT NULL,
+  `message_id` int(11) NOT NULL,
+  `user_own` varchar(15) NOT NULL,
+  `user_receive` varchar(15) NOT NULL,
   `content` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+
+--
+-- Dumping data for table `tb_message`
+--
+
+INSERT INTO `tb_message` (`message_id`, `user_own`, `user_receive`, `content`) VALUES
+(1, '01649214266', '0905242897', 'Xin chào!'),
+(2, '0905242897', '01649214266', 'Hi'),
+(3, '01649214266', '0905242897', 'What you name!'),
+(4, '0905242897', '01649214266', 'My name Hi');
 
 -- --------------------------------------------------------
 
@@ -57,11 +68,26 @@ INSERT INTO `tb_user` (`user_id`, `name`) VALUES
 --
 
 --
+-- Indexes for table `tb_message`
+--
+ALTER TABLE `tb_message`
+  ADD PRIMARY KEY (`message_id`);
+
+--
 -- Indexes for table `tb_user`
 --
 ALTER TABLE `tb_user`
   ADD PRIMARY KEY (`user_id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_message`
+--
+ALTER TABLE `tb_message`
+  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
